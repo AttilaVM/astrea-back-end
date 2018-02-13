0a4cc2b8641bba32b48deab7f5b73417b4b51d73
Attila Molnar <ate.molnar2@gmail.com>
2018-02-13 23:14:41 +0100

    Fix: init scrip

--- init.sh
+++ init.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Go into the script directory
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+# Install global dependencies
 npm install --global gulp rollup
 
+# build front end
+
+cd app/front-end
+
+npm install
+./setup.sh
+gulp template
+gulp style
+rollup -c rollup.dev.js
+
+cd ..
+
+# intall backend dependencies
 npm install
